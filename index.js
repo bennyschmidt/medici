7b5b3b1630fdd74f66690fc999c70ac94322fa56
exactchange <benny@exactchange.network>
2023-11-12 21:21:40 -0800

    tests

--- index.js
+++ index.js
@@ -5,35 +5,32 @@ const browser = new Medici();
 // Run some tests on different content types
 // served from peer "@exactchange"
 
-// setTimeout(async () => {
-//   await browser.onNavigate('@exactchange:text:example');
+setTimeout(async () => {
+  await browser.onNavigate('@exactchange:text:example');
 
-//   setTimeout(async () => {
-//     await browser.onNavigate('@exactchange:data:example');
+  setTimeout(async () => {
+    await browser.onNavigate('@exactchange:data:example');
 
-//     setTimeout(async () => {
-//       await browser.onNavigate('@exactchange:image:example');
+    setTimeout(async () => {
+      await browser.onNavigate('@exactchange:image:example');
 
-//       setTimeout(async () => {
-//         await browser.onNavigate('@exactchange:audio:example');
+      setTimeout(async () => {
+        await browser.onNavigate('@exactchange:audio:example');
 
-//         setTimeout(async () => {
-//           await browser.onNavigate('@exactchange:video:example');
+        setTimeout(async () => {
+          await browser.onNavigate('@exactchange:video:example');
 
-//           setTimeout(async () => {
-//             await browser.onNavigate('@exactchange:page:example');
+          setTimeout(async () => {
+            await browser.onNavigate('@exactchange:page:example');
 
-//             setTimeout(async () => {
-//               await browser.onNavigate('@exactchange:app:example');
-//             }, 2000);
-//           }, 2000);
-//         }, 2000);
-//       }, 2000);
-//     }, 2000);
-//   }, 2000);
-// }, 2000);
-
-setTimeout(() => {
-  browser.onNavigate('@exactchange:app:example');
+            setTimeout(async () => {
+              await browser.onNavigate('@exactchange:app:example');
+            }, 2000);
+          }, 2000);
+        }, 2000);
+      }, 2000);
+    }, 2000);
+  }, 2000);
 }, 2000);
+
 
